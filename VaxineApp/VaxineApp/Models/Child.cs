0dd0a46534332727b6c460596813e50741e7d180
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-04-25 14:56:06 +0430

    add: child model

---
+++ VaxineApp/VaxineApp/Models/Child.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VaxineApp.Models
+{
+    public class Child
+    {
+        public Guid Id { get; set; }
+        public string HouseNo { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string OPV0 { get; set; }
+        public string RINo { get; set; }
+        public UserMetaData UserMetaData { get; set; }
+        public Child()
+        {
+            Id = Guid.NewGuid();
+            UserMetaData = new UserMetaData();
+        }
+    }
+}
