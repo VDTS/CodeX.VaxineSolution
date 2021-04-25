71f53b25e738e63a8a5f181befae5afe667ac0ba
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-04-25 10:51:57 +0430

    add: Entry Custom renderer

---
+++ VaxineApp/VaxineApp.Android/Renderer/AndroidEntryRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VaxineApp.Droid.Renderer;
+using VaxineApp.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntryRenderer), typeof(AndroidEntryRenderer))]
+namespace VaxineApp.Droid.Renderer
+{
+    public class AndroidEntryRenderer : EntryRenderer
+    {
+        public AndroidEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                //Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+                var nativeEditText = (global::Android.Widget.EditText)Control;
+                var shape = new ShapeDrawable(new Android.Graphics.Drawables.Shapes.RectShape());
+                shape.Paint.Color = Xamarin.Forms.Color.Black.ToAndroid();
+                shape.Paint.SetStyle(Paint.Style.Stroke);
+                nativeEditText.Background = shape;
+            }
+        }
+    }
+}
+
+
+
