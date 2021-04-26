c540672c18152cd751df3ddea032381aea716038
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-04-26 16:25:03 +0430

    add: firebase database connection, and user secrets access class

---
+++ VaxineApp/VaxineApp/Services/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using VaxineApp.Models;
+
+namespace VaxineApp.Services
+{
+    public class DbContext
+    {
+        FirebaseClient firebase = new FirebaseClient(UserSecretsManager.Settings["firebase"]);
+        public async Task Add(ChildModel child)
+        {
+            await firebase
+              .Child("Child")
+              .PostAsync(child);
+        }
+    }
+}
