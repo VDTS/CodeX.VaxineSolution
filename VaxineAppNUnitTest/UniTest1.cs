4df9fca5afeb0f56aea4f6caf39803a2a8343eb1
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-05-08 08:15:18 +0430

    add: XamarinUITest and NUnitTest projects

---
+++ VaxineAppNUnitTest/UniTest1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace VaxineAppNUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SampleTest()
+        {
+            // Arrange
+            int a = 1;
+            int b = 2;
+
+            // Act
+            int c = a + b;
+            
+            // Assert
+            Assert.AreEqual(c, 3);
+        }
+    }
+}
---
+++ VaxineAppUITest/AppInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace VaxineAppUITest
+{
+    public class AppInitializer
+    {
+        // The Commented StartApp method was default method, and it doesn't work. 
+        // This stackoverflow solution suggested the below StartApp method.
+        // https://stackoverflow.com/questions/51296837/xamarin-uitesting-nu1201-error-android-8-1-is-incompatible-with-netframework-4
+        //public static IApp StartApp(Platform platform)
+        //{
+        //    if (platform == Platform.Android)
+        //    {
+        //        return ConfigureApp.Android.StartApp();
+        //    }
+
+        //    return ConfigureApp.iOS.StartApp();
+        //}
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.codex.vaxineapp").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
