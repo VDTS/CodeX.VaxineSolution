10f60cb15cfe6cfd4e3298df274f6a193fd8ea6d
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-05-25 20:07:13 +0430

    add: app center configured

--- VaxineApp/VaxineApp/App.xaml.cs
+++ VaxineApp/VaxineApp/App.xaml.cs
@@ -2,6 +2,9 @@
 using VaxineApp.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace VaxineApp
 {
@@ -16,6 +19,7 @@ namespace VaxineApp
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=518d8341-3aa8-467f-ae69-fdae9b224b1b;", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
