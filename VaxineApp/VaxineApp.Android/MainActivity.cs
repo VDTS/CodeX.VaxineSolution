02e8d508648ca939db529294b4ed3c903b795d59
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-05-27 19:37:46 +0430

    fix: MainActivity == true caused deploying two apps fixed

--- VaxineApp/VaxineApp.Android/MainActivity.cs
+++ VaxineApp/VaxineApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Firebase;
 
 namespace VaxineApp.Droid
 {
-    [Activity(Label = "VaxineApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "VaxineApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
