062d5a818b49ebc0c0e9dfd40047dd1ae43a914f
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-03 15:23:27 +0430

    fixed: SecretsVault path fixed

--- VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
+++ VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
@@ -6,5 +6,5 @@ then
 /usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VDTSXamarin.$APPCENTER_BRANCH" "droid/AndroidManifest"
 
 fi
-
-sed -i -e "s/\[VSAC_GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" ../VaxineApp/SecretsVault.cs
+APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxineApp/SecretsVault.cs
+sed -i -e "s/\[VSAC_GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" $APP_CONSTANT_FILE
