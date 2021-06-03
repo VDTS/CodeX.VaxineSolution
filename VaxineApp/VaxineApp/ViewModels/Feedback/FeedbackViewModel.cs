e0c6d948bb8e21cb1fe607df7689a299ebce64d9
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-03 09:42:14 +0430

    add: Creating issue on github RestApi

--- VaxineApp/VaxineApp/ViewModels/Feedback/FeedbackViewModel.cs
+++ VaxineApp/VaxineApp/ViewModels/Feedback/FeedbackViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Octokit;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 using VaxineApp.ViewModels.Base;
 using VaxineApp.Views.Home.Status;
 using Xamarin.Forms;
+using Octokit.Internal;
+using System.Collections.ObjectModel;
 
 namespace VaxineApp.ViewModels.Feedback
 {
@@ -21,14 +24,25 @@ namespace VaxineApp.ViewModels.Feedback
                 RaisedPropertyChanged(nameof(IssueTitle));
             }
         }
-        private string _suggestionRadioButton;
-        public string SuggestionRadioButton
+        private bool _suggestionRadioButton;
+        public bool SuggestionRadioButton
         {
             get { return _suggestionRadioButton; }
             set
             {
                 _suggestionRadioButton = value;
                 RaisedPropertyChanged(nameof(SuggestionRadioButton));
+            }
+        }
+
+        private bool _problemRadioButton;
+        public bool ProblemRadioButton
+        {
+            get { return _problemRadioButton; }
+            set
+            {
+                _problemRadioButton = value;
+                RaisedPropertyChanged(nameof(ProblemRadioButton));
             }
         }
 
@@ -50,8 +64,37 @@ namespace VaxineApp.ViewModels.Feedback
 
         private async void SubmitIssueOnGithub()
         {
+            SubmitIssue();
             var route = $"//{nameof(StatusPage)}";
             await Shell.Current.GoToAsync(route);
+        }
+
+        private async void SubmitIssue()
+        {
+            try
+            {
+                var client = new GitHubClient(new ProductHeaderValue("VDTSApps"));
+
+                var tokenAuth = new Credentials(SecretsVault.GithubApiKeyForCreatingIssues);
+                client.Credentials = tokenAuth;
+
+                var i = new NewIssue(IssueTitle);
+                i.Body = IssueDetails;
+                if(SuggestionRadioButton == false && ProblemRadioButton == true)
+                {
+                    i.Labels.Add("Problem");
+                }
+                else
+                {
+                    i.Labels.Add("Suggestion");
+                }
+
+                var issue = await client.Issue.Create("VDTS", "VDTSApps", i);
+            }
+            catch (Exception)
+            {
+                await App.Current.MainPage.DisplayAlert("Issue not submitted", "try again!", "OK");
+            }
         }
     }
 }
