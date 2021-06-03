5abc8ab74d2a706a5e314a35a8a990f66bed1e50
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-03 10:54:45 +0430

    add: app center pre build script

---
+++ VaxineApp/VaxineApp/SecretsVault.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp
+{
+    public static class SecretsVault
+    {
+        public static string GithubApiKeyForCreatingIssues { get; set; }
+    }
+}
