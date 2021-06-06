c737a459fc164211c81137682421a7138db4361a
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-06 19:53:01 +0430

    mod: Clinic rest Api

---
+++ DataAccessLib/Models/ProfileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLib.Models
+{
+    public class ProfileModel
+    {
+        // Image property
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string FatherOrHusbandName { get; set; }
+        public int Age { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Team { get; set; }
+        public string Cluster { get; set; }
+        public string Area { get; set; }
+    }
+}
