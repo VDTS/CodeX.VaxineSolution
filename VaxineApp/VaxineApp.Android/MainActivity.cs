53fb03ef28d0f1c63c536e4096e8d73c213d1ef0
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-08 23:45:36 +0430

    add: Imag gallery browser for profile photo

--- VaxineApp/VaxineApp.Android/MainActivity.cs
+++ VaxineApp/VaxineApp.Android/MainActivity.cs
@@ -5,14 +5,19 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 using Firebase;
+using System.IO;
+using Android.Content;
+using System.Threading.Tasks;
 
 namespace VaxineApp.Droid
 {
     [Activity(Label = "VaxineApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static MainActivity Instance { get; private set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Instance = this;
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -25,6 +30,31 @@ namespace VaxineApp.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        // Field, property, and method for Picture Picker
+        public static readonly int PickImageId = 1000;
+
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { set; get; }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = ContentResolver.OpenInputStream(uri);
+
+                    // Set the Stream as the completion of the Task
+                    PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
         }
     }
 }
---
+++ VaxineApp/VaxineApp.Android/NativeApi/PhotoPickerService.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaxineApp.AndroidNativeApi;
+using VaxineApp.Droid.NativeApi;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhotoPickerService))]
+namespace VaxineApp.Droid.NativeApi
+{
+    public class PhotoPickerService : IPhotoPickerService
+    {
+        public Task<Stream> GetImageStreamAsync()
+        {
+            // Define the Intent for getting images
+            Intent intent = new Intent();
+            intent.SetType("image/*");
+            intent.SetAction(Intent.ActionGetContent);
+
+            // Start the picture-picker activity (resumes in MainActivity.cs)
+            MainActivity.Instance.StartActivityForResult(
+                Intent.CreateChooser(intent, "Select Picture"),
+                MainActivity.PickImageId);
+
+            // Save the TaskCompletionSource object as a MainActivity property
+            MainActivity.Instance.PickImageTaskCompletionSource = new TaskCompletionSource<Stream>();
+
+            // Return Task object
+            return MainActivity.Instance.PickImageTaskCompletionSource.Task;
+        }
+    }
+}
