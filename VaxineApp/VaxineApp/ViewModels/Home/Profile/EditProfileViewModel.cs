11884f4f0b3f86df1f21ed779ed8088e6441d67b
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-08 23:45:36 +0430

    add: Imag gallery browser for profile photo

--- VaxineApp/VaxineApp/ViewModels/Home/Profile/EditProfileViewModel.cs
+++ VaxineApp/VaxineApp/ViewModels/Home/Profile/EditProfileViewModel.cs
@@ -2,8 +2,10 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows.Input;
+using VaxineApp.AndroidNativeApi;
 using VaxineApp.ViewModels.Base;
 using VaxineApp.Views.Home.Profile;
 using Xamarin.Forms;
@@ -14,10 +16,12 @@ namespace VaxineApp.ViewModels.Home.Profile
     {
         public ProfileModel Profile { get; set; }
         public ICommand SaveDataCommand { private set; get; }
+        public ICommand OpenFileBrowserCommad { private set; get; }
         public EditProfileViewModel(ProfileModel _profile)
         {
             Profile = _profile;
             SaveDataCommand = new Command(SaveData);
+            OpenFileBrowserCommad = new Command(OpenFileBrowser);
         }
 
         async void SaveData(object obj)
@@ -31,6 +35,17 @@ namespace VaxineApp.ViewModels.Home.Profile
             //await Shell.Current.GoToAsync(route);
             await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
 
+        }
+        public async void OpenFileBrowser(object sender)
+        {
+            //(sender as Button).IsEnabled = false;
+            Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+            if (stream != null)
+            {
+                await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
+                //image.Source = ImageSource.FromStream(() => stream);
+            }
+            //(sender as Button).IsEnabled = true;
         }
     }
 }
