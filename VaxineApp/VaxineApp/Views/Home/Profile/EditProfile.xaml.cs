c7ee770798640fd8079ec7f8e8dc10e28ff8385c
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-08 23:15:53 +0430

    add: EditProfileViewModel

--- VaxineApp/VaxineApp/Views/Home/Profile/EditProfile.xaml.cs
+++ VaxineApp/VaxineApp/Views/Home/Profile/EditProfile.xaml.cs
@@ -1,20 +1,30 @@
-﻿using System;
+﻿using DataAccessLib.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VaxineApp.ViewModels.Home.Profile;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VaxineApp.Views.Home.Profile
 {
+    [QueryProperty(nameof(Profile), nameof(Profile))]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EditProfile : ContentPage
     {
+        public string Profile { get; set; }
         public EditProfile()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            var result = JsonConvert.DeserializeObject<ProfileModel>(Profile);
+            BindingContext = new EditProfileViewModel(result);
         }
     }
 }
