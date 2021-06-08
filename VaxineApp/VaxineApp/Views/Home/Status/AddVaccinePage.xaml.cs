3db8af398243f5616761393e37bc4214df96820d
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-08 12:06:38 +0430

    fix: routing ChildVaccinePage -> AddVaccinePage

--- VaxineApp/VaxineApp/Views/Home/Status/AddVaccinePage.xaml.cs
+++ VaxineApp/VaxineApp/Views/Home/Status/AddVaccinePage.xaml.cs
@@ -1,21 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VaxineApp.Models;
 using VaxineApp.ViewModels.Home.Status;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VaxineApp.Views.Home.Status
 {
+    [QueryProperty(nameof(Child), nameof(Child))]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddVaccinePage : ContentPage
     {
-        public AddVaccinePage(Guid ChildId)
+        public string Child { get; set; }
+        public AddVaccinePage()
         {
             InitializeComponent();
-            BindingContext = new AddVaccineViewModel(ChildId);
+        }
+        protected override void OnAppearing()
+        {
+            var result = JsonConvert.DeserializeObject<ChildModel>(Child);
+            BindingContext = new AddVaccineViewModel(result);
         }
     }
 }
