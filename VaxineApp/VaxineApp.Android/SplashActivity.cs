0e8956fce4dabe9eb6af7793deccc3b70bec672e
Naveed Ahmad Hematmal <38671143+NaveedAhmadHematmal@users.noreply.github.com>
2021-06-13 19:39:41 +0430

    Update SplashActivity.cs

--- VaxineApp/VaxineApp.Android/SplashActivity.cs
+++ VaxineApp/VaxineApp.Android/SplashActivity.cs
@@ -16,10 +16,57 @@ namespace com.xamarin.sample.splashscreen
 
         public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
         {
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
+            Distribute.NoReleaseAvailable = OnNoReleaseAvailable;
+            Distribute.SetEnabledForDebuggableBuild(true);
+            AppCenter.Start("android=518d8341-3aa8-467f-ae69-fdae9b224b1b;", typeof(Analytics), typeof(Crashes), typeof(Distribute));
             base.OnCreate(savedInstanceState, persistentState);
             Log.Debug(TAG, "SplashActivity.OnCreate");
         }
+bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            // Look at releaseDetails public properties to get version information, release notes text or release notes URL
+            string versionName = releaseDetails.ShortVersion;
+            string versionCodeOrBuildNumber = releaseDetails.Version;
+            string releaseNotes = releaseDetails.ReleaseNotes;
+            Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
 
+            // custom dialog
+            var title = "Version " + versionName + " available!";
+            Task answer;
+
+            // On mandatory update, user can't postpone
+            if (releaseDetails.MandatoryUpdate)
+            {
+                answer = App.Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
+            }
+            else
+            {
+                answer = App.Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
+            }
+            answer.ContinueWith((task) =>
+            {
+                // If mandatory or if answer was positive
+                if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
+                {
+                    // Notify SDK that user selected update
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
+                }
+                else
+                {
+                    // Notify SDK that user selected postpone (for 1 day)
+                    // This method call is ignored by the SDK if the update is mandatory
+                    Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                }
+            });
+
+            // Return true if you're using your own dialog, false otherwise
+            return true;
+        }
+        async void OnNoReleaseAvailable()
+        {
+            await App.Current.MainPage.DisplayAlert("No updates found", "Try later", "OK");
+        }
         // Launches the startup task
         protected override void OnResume()
         {
