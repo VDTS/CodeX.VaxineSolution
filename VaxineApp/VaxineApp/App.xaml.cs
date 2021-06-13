6803370bfa596f2df1f0003718b76e13af82f641
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-13 14:36:34 +0430

    add: app center in-app update messages

--- VaxineApp/VaxineApp/App.xaml.cs
+++ VaxineApp/VaxineApp/App.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using VaxineApp.Views.Login;
 using Microsoft.AppCenter.Distribute;
+using System.Threading.Tasks;
 
 namespace VaxineApp
 {
@@ -29,9 +30,54 @@ namespace VaxineApp
 
         protected override void OnStart()
         {
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
+            Distribute.NoReleaseAvailable = OnNoReleaseAvailable;
             AppCenter.Start("android=518d8341-3aa8-467f-ae69-fdae9b224b1b;", typeof(Analytics), typeof(Crashes), typeof(Distribute));
         }
+        bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            // Look at releaseDetails public properties to get version information, release notes text or release notes URL
+            string versionName = releaseDetails.ShortVersion;
+            string versionCodeOrBuildNumber = releaseDetails.Version;
+            string releaseNotes = releaseDetails.ReleaseNotes;
+            Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
 
+            // custom dialog
+            var title = "Version " + versionName + " available!";
+            Task answer;
+
+            // On mandatory update, user can't postpone
+            if (releaseDetails.MandatoryUpdate)
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
+            }
+            else
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
+            }
+            answer.ContinueWith((task) =>
+            {
+                // If mandatory or if answer was positive
+                if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
+                {
+                    // Notify SDK that user selected update
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
+                }
+                else
+                {
+                    // Notify SDK that user selected postpone (for 1 day)
+                    // This method call is ignored by the SDK if the update is mandatory
+                    Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                }
+            });
+
+            // Return true if you're using your own dialog, false otherwise
+            return true;
+        }
+        async void OnNoReleaseAvailable()
+        {
+            await App.Current.MainPage.DisplayAlert("No updates found", "Try later", "OK");
+        }
         protected override void OnSleep()
         {
         }
