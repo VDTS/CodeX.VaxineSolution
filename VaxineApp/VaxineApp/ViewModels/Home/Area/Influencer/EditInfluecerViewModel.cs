ae0c3e2e6f785180bb29badbfb8d41eae6c4b6ef
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-15 11:47:17 +0430

    add: Influencer edit page

---
+++ VaxineApp/VaxineApp/ViewModels/Home/Area/Influencer/EditInfluecerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLib.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using VaxineApp.ViewModels.Base;
+using VaxineApp.Views.Home.Area.Influencer;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace VaxineApp.ViewModels.Home.Area.Influencer
+{
+    public class EditInfluecerViewModel : BaseViewModel
+    {
+        private InfluencerModel _influencer;
+        public InfluencerModel Influencer
+        {
+            get { return _influencer; }
+            set
+            {
+                _influencer = value;
+                RaisedPropertyChanged(nameof(Influencer));
+            }
+        }
+        public ICommand UpdateInfluencerCommand { private set; get; }
+
+        public EditInfluecerViewModel(InfluencerModel influencer)
+        {
+            Influencer = influencer;
+            UpdateInfluencerCommand = new Command(UpdateInfluencer);
+        }
+
+        private async void UpdateInfluencer(object obj)
+        {
+            await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
+        }
+    } 
+}
