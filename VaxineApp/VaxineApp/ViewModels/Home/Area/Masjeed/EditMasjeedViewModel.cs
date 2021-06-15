689aaceb9af4c3fcc16a8153c0fdd82a3f14c408
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-15 12:02:06 +0430

    add: Masjeed edit page

---
+++ VaxineApp/VaxineApp/ViewModels/Home/Area/Masjeed/EditMasjeedViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLib.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using VaxineApp.ViewModels.Base;
+using VaxineApp.Views.Home.Area.Masjeed;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace VaxineApp.ViewModels.Home.Area.Masjeed
+{
+    public class EidtMasjeedViewModel : BaseViewModel
+    {
+        private MasjeedModel _masjeed;
+        public MasjeedModel Masjeed
+        {
+            get { return _masjeed; }
+            set
+            {
+                _masjeed = value;
+                RaisedPropertyChanged(nameof(Masjeed));
+            }
+        }
+        public ICommand UpdateMasjeedCommand { private set; get; }
+
+        public EidtMasjeedViewModel(MasjeedModel masjeed)
+        {
+            Masjeed = masjeed;
+            UpdateMasjeedCommand = new Command(UpdateMasjeed);
+        }
+
+        private async void UpdateMasjeed(object obj)
+        {
+            await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
+        }
+    }
+}
