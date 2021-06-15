fca146af1bcda91595e3a093e2a5a84cd1c80dda
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-15 12:24:02 +0430

    add: School edit page

---
+++ VaxineApp/VaxineApp/ViewModels/Home/Area/School/EditSchoolViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLib.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using VaxineApp.ViewModels.Base;
+using VaxineApp.Views.Home.Area.School;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace VaxineApp.ViewModels.Home.Area.School
+{
+    public class EditSchoolViewModel : BaseViewModel
+    {
+        private SchoolModel _school;
+        public SchoolModel School
+        {
+            get { return _school; }
+            set
+            {
+                _school = value;
+                RaisedPropertyChanged(nameof(School));
+            }
+        }
+        public ICommand UpdateSchoolCommand { private set; get; }
+
+        public EditSchoolViewModel(SchoolModel school)
+        {
+            School = school;
+            UpdateSchoolCommand = new Command(UpdateSchool);
+        }
+
+        private async void UpdateSchool(object obj)
+        {
+            await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
+        }
+    }
+}
---
+++ VaxineApp/VaxineApp/Views/Home/Area/School/EditSchoolPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLib.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaxineApp.ViewModels.Home.Area.School;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VaxineApp.Views.Home.Area.School
+{
+    [QueryProperty(nameof(School), nameof(School))]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditSchoolPage : ContentPage
+    {
+        public string School { get; set; }
+        public EditSchoolPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            var result = JsonConvert.DeserializeObject<SchoolModel>(School);
+            BindingContext = new EditSchoolViewModel(result);
+        }
+    }
+}
