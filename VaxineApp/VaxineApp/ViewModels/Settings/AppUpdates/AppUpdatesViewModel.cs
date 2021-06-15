e1b6c96b023c477853ffa9d494b42c183fe885e4
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-15 14:40:55 +0430

    fix: release page fixed

--- VaxineApp/VaxineApp/ViewModels/Settings/AppUpdates/AppUpdatesViewModel.cs
+++ VaxineApp/VaxineApp/ViewModels/Settings/AppUpdates/AppUpdatesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AppCenter.Distribute;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,56 +13,41 @@ namespace VaxineApp.ViewModels.Settings.AppUpdates
 {
     public class AppUpdatesViewModel : BaseViewModel
     {
-        public ICommand CheckForUpdateCommand { private set; get; }
-        private string _version;
-        public string Version
+        private bool _isUpdatesAvailable;
+
+        public bool IsUpdatesAvailable
         {
-            get { return _version; }
+            get { return _isUpdatesAvailable; }
             set
             {
-                _version = value;
-                RaisedPropertyChanged(nameof(Version));
-            }
-        }
-        private string _release;
-        public string Release
-        {
-            get { return _release; }
-            set
-            {
-                _release = value;
-                RaisedPropertyChanged(nameof(Release));
+                _isUpdatesAvailable = value;
+                RaisedPropertyChanged(nameof(IsUpdatesAvailable));
             }
         }
 
-        private string _buildNo;
-        public string BuildNo
+        private Root _appNewUpdates;
+        public Root AppNewUpdates
         {
-            get { return _buildNo; }
+            get { return _appNewUpdates; }
             set
             {
-                _buildNo = value;
-                RaisedPropertyChanged(nameof(BuildNo));
+                _appNewUpdates = value;
+                RaisedPropertyChanged(nameof(AppNewUpdates));
             }
         }
-        private string _whatsNewContent;
-        public string WhatsNewContent
-        {
-            get { return _whatsNewContent; }
-            set
-            {
-                _whatsNewContent = value;
-                RaisedPropertyChanged(nameof(WhatsNewContent));
-            }
-        }
+
+        public ICommand CheckForUpdateCommand { private set; get; }
+        public ICommand TapToGoToPageCommand { private set; get; }
         public AppUpdatesViewModel()
         {
             CheckForUpdateCommand = new Command(CheckForUpdate);
-            Version = "1.0 - alpha";
-            BuildNo = "46";
-            Release = "35";
-
+            TapToGoToPageCommand = new Command<string>(TapToGoToPage);
             DownloadFile();
+        }
+
+        private async void TapToGoToPage(string url)
+        {
+            await App.Current.MainPage.DisplayAlert($"{url}", "Hyperlink to the page is under development", "OK");
         }
 
         private void CheckForUpdate(object obj)
@@ -71,17 +57,33 @@ namespace VaxineApp.ViewModels.Settings.AppUpdates
 
         public void DownloadFile()
         {
-            try{
-                    WebClient client = new WebClient();
-                    Stream stream = client.OpenRead("https://raw.githubusercontent.com/VDTS/docs/main/ReleaseNotes/35.txt");
-                    StreamReader reader = new StreamReader(stream);
-                    WhatsNewContent = reader.ReadToEnd();
-                 }
-                catch (Exception)
-                {
-                    WhatsNewContent = $"No release note available for this release, for earlier versions release note, see https://github.com/VDTS/docs/tree/main/ReleaseNotes";
-                }
+            try
+            {
+                WebClient client = new WebClient();
+                Stream stream = client.OpenRead("https://raw.githubusercontent.com/VDTS/docs/main/AndroidReleaseNotes/37.json");
+                StreamReader reader = new StreamReader(stream);
+                string data = reader.ReadToEnd();
+                AppNewUpdates = JsonConvert.DeserializeObject<Root>(data);
+            }
+            catch (Exception)
+            {
+                IsUpdatesAvailable = true;
+            }
         }
+    }
+    public class Content
+    {
+        public string Location { get; set; }
+        public string Updates { get; set; }
+    }
+
+    public class Root
+    {
+        public string Version { get; set; }
+        public string Build { get; set; }
+        public string ReleaseNo { get; set; }
+        public string ReleaseBranch { get; set; }
+        public List<Content> Content { get; set; }
     }
 }
 
