aeddfb417380f1d207bb7b9f72485fcad6d6d5ca
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-15 12:37:54 +0430

    add: Font, Notifications, Language and Themes pages

---
+++ VaxineApp/VaxineApp/Views/Settings/Font/FontPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VaxineApp.Views.Settings.Font
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FontPage : ContentPage
+    {
+        public FontPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
