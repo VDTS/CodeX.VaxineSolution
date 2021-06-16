950f298d49d086084cef86d61fd1ae1a4ad8eaca
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-16 23:14:28 +0430

    add: Local Push Notifications

---
+++ VaxineApp/VaxineApp.Android/NativeApi/AlarmHandler.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VaxineApp.Droid.NativeApi
+{
+    [BroadcastReceiver(Enabled = true, Label = "Local Notifications Broadcast Receiver")]
+    public class AlarmHandler : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent?.Extras != null)
+            {
+                string title = intent.GetStringExtra(AndroidNotificationManager.TitleKey);
+                string message = intent.GetStringExtra(AndroidNotificationManager.MessageKey);
+
+                AndroidNotificationManager manager = AndroidNotificationManager.Instance ?? new AndroidNotificationManager();
+                manager.Show(title, message);
+            }
+        }
+    }
+}
---
+++ VaxineApp/VaxineApp/AndroidNativeApi/INotificationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.AndroidNativeApi
+{
+    public interface INotificationManager
+    {
+        event EventHandler NotificationReceived;
+        void Initialize();
+        void SendNotification(string title, string message, DateTime? notifyTime = null);
+        void ReceiveNotification(string title, string message);
+    }
+}
