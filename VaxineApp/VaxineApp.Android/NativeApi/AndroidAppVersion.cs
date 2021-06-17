3feb096f64f489bda65986674a4b32456fde83f3
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-17 08:55:23 +0430

    add: versionname to ReleasePage

---
+++ VaxineApp/VaxineApp.Android/NativeApi/AndroidAppVersion.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VaxineApp.AndroidNativeApi;
+
+[assembly: Xamarin.Forms.Dependency(typeof(VaxineApp.Droid.NativeApi.AndroidAppVersion))]
+namespace VaxineApp.Droid.NativeApi
+{
+    public class AndroidAppVersion : IAppVersion
+    {
+        public string GetVersion()
+        {
+            var context = global::Android.App.Application.Context;
+
+            PackageManager manager = context.PackageManager;
+            PackageInfo info = manager.GetPackageInfo(context.PackageName, 0);
+
+            return info.VersionName;
+        }
+
+        public int GetBuild()
+        {
+            var context = global::Android.App.Application.Context;
+            PackageManager manager = context.PackageManager;
+            PackageInfo info = manager.GetPackageInfo(context.PackageName, 0);
+
+            return info.VersionCode;
+        }
+    }
+}
---
+++ VaxineApp/VaxineApp/AndroidNativeApi/IAppVersion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.AndroidNativeApi
+{
+    public interface IAppVersion
+    {
+        string GetVersion();
+        int GetBuild();
+    }
+}
