f9e59e2015ab6097f5bf571257a0afd3447b4e16
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-17 06:57:12 +0430

    add: Family edit page

---
+++ VaxineApp/VaxineApp/ViewModels/Home/Family/EditFamilyViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using VaxineApp.Models;
+using VaxineApp.ViewModels.Base;
+using VaxineApp.Views.Home.Family;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace VaxineApp.ViewModels.Home.Family
+{
+    public class EditFamilyViewModel : BaseViewModel
+    {
+        private GetFamilyModel _family;
+
+        public GetFamilyModel Family
+        {
+            get { return _family; }
+            set
+            {
+                _family = value;
+                RaisedPropertyChanged(nameof(Family));
+            }
+        }
+
+
+        // Commands
+
+        public ICommand UpdateFamilyCommand { private set; get; }
+        // Constructor
+        public EditFamilyViewModel(GetFamilyModel family)
+        {
+            Family = family;
+            UpdateFamilyCommand = new Command(UpdateFamily);
+        }
+        private async void UpdateFamily(object obj)
+        {
+            await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
+        }
+    }
+}
