3ce96cac8c00fd7cce3349c3ff9e6339cc17db65
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-19 09:57:03 +0430

    add: edit functionality to clinic page

--- VaxineApp/VaxineApp/ViewModels/Home/Area/Clinic/EditClinicViewModel.cs
+++ VaxineApp/VaxineApp/ViewModels/Home/Area/Clinic/EditClinicViewModel.cs
@@ -1,9 +1,9 @@
-﻿using DataAccessLib.Models;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using VaxineApp.Models;
 using VaxineApp.ViewModels.Base;
 using VaxineApp.Views.Home.Area.Clinic;
 using Xamarin.Essentials;
@@ -33,7 +33,18 @@ namespace VaxineApp.ViewModels.Home.Area.Clinic
 
         private async void UpdateClinic(object obj)
         {
-            await App.Current.MainPage.DisplayAlert("Not submitted!", "This functionality is under construction", "OK");
+            Clinic.ClinicName = "New";
+            var jsonData = JsonConvert.SerializeObject(Clinic);
+            var data = await DataService.Put(jsonData, $"Clinic/{Preferences.Get("TeamId", "")}/{Clinic.FId}");
+            if (data == "Not updated")
+            {
+                await App.Current.MainPage.DisplayAlert("Updated", $"item has been updated", "OK");
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Not Updated", "Try again", "OK");
+            }
+
         }
     }
 }
