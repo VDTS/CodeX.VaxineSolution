74c43131f17721f8d832a6d36b02cfe754b5ae8f
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-27 15:57:04 +0430

    fix: Removed unwanted data in profile

--- DataAccessLib/Models/ProfileModel.cs
+++ DataAccessLib/Models/ProfileModel.cs
@@ -7,16 +7,15 @@ namespace DataAccessLib.Models
     public class ProfileModel
     {
         // Image property
+        public string FId { get; set; }
         public Guid Id { get; set; }
         public string FullName { get; set; }
         public string Gender { get; set; }
         public string FatherOrHusbandName { get; set; }
         public int Age { get; set; }
         public string Role { get; set; }
-        public string Email { get; set; }
         public string TeamId { get; set; }
         public string ClusterId { get; set; }
-        public string PhotoURL { get; set; }
         public ProfileModel()
         {
             Id = new Guid();
