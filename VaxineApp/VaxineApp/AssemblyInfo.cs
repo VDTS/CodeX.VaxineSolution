93ec0fe9efcf1ffe0880b07ba2584d90b7fb7df8
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-06-28 16:42:08 +0430

    add: MaterialDesignIcons

--- VaxineApp/VaxineApp/AssemblyInfo.cs
+++ VaxineApp/VaxineApp/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("segmdl2.ttf", Alias = "SegoeMDL2")]
+[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "MaterialIcons")]
