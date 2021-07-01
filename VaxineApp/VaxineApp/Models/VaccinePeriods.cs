3c130d5614d3e3a63038de49f46c0f04b52723e1
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-01 20:24:44 +0430

    add: VaccinePeriods and VaccinePeriodId to ClusterModel

---
+++ VaxineApp/VaxineApp/Models/VaccinePeriods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.Models
+{
+    public class VaccinePeriods
+    {
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string PeriodName { get; set; }
+        public VaccinePeriods()
+        {
+            Id = new Guid();
+        }
+    }
+}
