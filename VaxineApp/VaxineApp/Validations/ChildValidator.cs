d12c903a3600e54bc75f4e9e8e1b8cb3a77d7e4b
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-01 15:05:12 +0430

    add: ChildValidator class

---
+++ VaxineApp/VaxineApp/Validations/ChildValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.Validations
+{
+    public static class ChildValidator
+    {
+        public static bool IsChildUnder5(DateTime DOB)
+        {
+            var ageInMonths = 12 * (DateTime.UtcNow.Year - DOB.Year) + DOB.Month;
+            if (ageInMonths <= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
