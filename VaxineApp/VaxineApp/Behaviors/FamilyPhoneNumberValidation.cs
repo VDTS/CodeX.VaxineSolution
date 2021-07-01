e85dc91a00e5da54d1e8362aeba3ecca5d90c9c0
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-01 18:08:18 +0430

    add: PhoneNumber validation and validation behavior to AddFamily page

---
+++ VaxineApp/VaxineApp/Behaviors/FamilyPhoneNumberValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VaxineApp.Validations;
+using Xamarin.Forms;
+
+namespace VaxineApp.Behaviors
+{
+    public class FamilyPhoneNumberValidation : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry emailEntry)
+        {
+            emailEntry.TextChanged += PhoneNumberEntryChanged;
+            base.OnAttachedTo(emailEntry);
+        }
+
+        private void PhoneNumberEntryChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = (Entry)sender;
+            if (PhoneNumberValidator.IsPhoneNumberValid(entry.Text))
+            {
+                entry.TextColor = Color.Black;
+            }
+            else
+            {
+                entry.TextColor = Color.Red;
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry emailEntry)
+        {
+            emailEntry.TextChanged += PhoneNumberEntryChanged;
+            base.OnDetachingFrom(emailEntry);
+        }
+    }
+}
---
+++ VaxineApp/VaxineApp/Validations/PhoneNumberValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace VaxineApp.Validations
+{
+    public class PhoneNumberValidator
+    {
+        public static bool IsPhoneNumberValid(string phoneNumber)
+        {
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                string phoneNumberWithInternationalCode = @"^[\+][9][3][0-9]{9}$";
+                string phoneNumberWithLocalCode = @"^[0][7][0-9]{8}$";
+                string phoneNumberWithCode = @"^[0]{2}[9][3][7][0-9]{8}$";
+
+                bool isphoneNumberWithInternationalCodeMatched = Regex.IsMatch(phoneNumber, phoneNumberWithInternationalCode);
+                bool isphoneNumberWithLocalCode = Regex.IsMatch(phoneNumber, phoneNumberWithLocalCode);
+                bool isphoneNumberWithCode = Regex.IsMatch(phoneNumber, phoneNumberWithCode);
+
+                if (isphoneNumberWithInternationalCodeMatched || isphoneNumberWithLocalCode || isphoneNumberWithCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
