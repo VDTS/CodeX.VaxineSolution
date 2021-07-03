c62373bc8bd15dbf0696b0bb8889230250d2932c
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-03 19:17:03 +0430

    add: Announcements centre

---
+++ VaxineApp/VaxineApp/Views/Announcements/AnnouncementsCentrePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VaxineApp.Views.Announcements
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AnnouncementsCentrePage : ContentPage
+    {
+        public AnnouncementsCentrePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
