6752da303d9b7c04d108dc001f8555dc1b545407
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-05 15:29:40 +0430

    fix: sed command

--- VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
+++ VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
@@ -8,7 +8,7 @@ then
 fi
 
 # Changing the variables in VaxineApp
-sed -i -e "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" ../VaxineApp/Constants.cs
+sed -i '' "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" ../VaxineApp/Constants.cs
 
 # Changing the variables in DataAccessLib
-sed -i -e "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../../DataAccessLib/Constants.cs
+sed -i '' "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../../DataAccessLib/Constants.cs
