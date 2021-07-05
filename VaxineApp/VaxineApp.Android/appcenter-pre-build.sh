ad1712af7663def8cd5324dda8604245bad1b41f
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-05 15:12:49 +0430

    adds Firebase key and url to app center environment

--- VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
+++ VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
@@ -7,4 +7,8 @@ then
 
 fi
 
-sed -i -e "s/\[VSAC_GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g; s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g" ../VaxineApp/Constants.cs
+# Changing the variables in VaxineApp
+sed -i -e "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" ../VaxineApp/Constants.cs
+
+# Changing the variables in DataAccessLib
+sed -i -e "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../../DataAccessLib/Constants.cs
--- VaxineApp/VaxineApp/Constants.cs
+++ VaxineApp/VaxineApp/Constants.cs
@@ -6,8 +6,7 @@ namespace VaxineApp
 {
     public static class Constants
     {
-        public static string GithubApiKeyForCreatingIssues { get; set; } = "[VSAC_GITHUB_API_KEY]";
+        public static string GithubApiKeyForCreatingIssues { get; set; } = "[GITHUB_API_KEY]";
         public static string SyncFusionCommunityLicenseKey { get; set; } = "[SF_COMMUNITY_LICENSE_KEY]";
-        public static string FirebaseApiKey { get; set; } = "[FB_API_KEY]";
     }
 }
