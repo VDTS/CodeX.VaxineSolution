cb3726a5e49f8a5a499b3d5fa88edfb3159e4f71
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-06 12:30:34 +0430

    appcenter-pre-build fixed and version increments

--- VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
+++ VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
@@ -8,4 +8,4 @@ then
 fi
 
 # Changing the variables in VaxineApp
-sed -i -e "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" ../VaxineApp/Constants.cs -e "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../../DataAccessLib/Constants.cs
+sed -i -e "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" -e "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../VaxineApp/Constants.cs ../../DataAccessLib/Constants.cs
