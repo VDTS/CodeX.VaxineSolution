fecdebc42892eb48c4e98b188f02e336686afc88
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-07 14:18:17 +0430

    appcenter-pre-build.sh fixed (semi colon added to each line)

--- VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
+++ VaxineApp/VaxineApp.Android/appcenter-pre-build.sh
@@ -8,4 +8,7 @@ then
 fi
 
 # Changing the variables in VaxineApp
-sed -i -e "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" -e "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../VaxineApp/Constants.cs ../../DataAccessLib/Constants.cs
+sed -i "s/\[GITHUB_API_KEY]/$VSAC_GITHUB_API_KEY/g" ../VaxineApp/Constants.cs;
+
+# Changing the variables in DataAccessLib
+sed -i "s/\[FB_API_KEY]/$VSAC_FB_API_KEY/g; s/\[FB_URL]/$VSAC_FB_URL/g" ../../DataAccessLib/Constants.cs;
