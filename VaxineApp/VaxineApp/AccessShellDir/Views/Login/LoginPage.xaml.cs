5d950f8de8c2637e2d5eb33a72bdb59cae62de13
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-08 15:48:26 +0430

    add show/hide password button to loginpage

--- VaxineApp/VaxineApp/AccessShellDir/Views/Login/LoginPage.xaml.cs
+++ VaxineApp/VaxineApp/AccessShellDir/Views/Login/LoginPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VaxineApp.Font;
 using VaxineApp.Views.Home.Family;
 using VaxineApp.Views.Home.Status;
 using Xamarin.Forms;
@@ -16,6 +17,20 @@ namespace VaxineApp.AccessShellDir.Views.Login
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void ShowHidePassword(object sender, EventArgs e)
+        {
+            if (PasswordField.IsPassword == true)
+            {
+                PasswordField.IsPassword = false;
+                ShowHideButton.Text = MaterialDesignIcons.Eye;
+            }
+            else
+            {
+                PasswordField.IsPassword = true;
+                ShowHideButton.Text = MaterialDesignIcons.EyeOff;
+            }
         }
     }
 }
