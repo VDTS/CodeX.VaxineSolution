ab41b9e6e923473aadf597686251064aa157df26
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-08 05:40:27 +0430

    appcenter fixed

--- DataAccessLib/Constants.cs
+++ DataAccessLib/Constants.cs
@@ -6,7 +6,7 @@ namespace VaxineApp.DataAccessLib
 {
     public static class Constants
     {
-        public static string ApiUrl { get; set; } = "ApiUrl";
+        public static string FirebaseApiKey { get; set; } = "FirebaseApiKey";
         public static string FirebaseBaseUrl { get; set; } = "[FB_URL]";
     }
 }
