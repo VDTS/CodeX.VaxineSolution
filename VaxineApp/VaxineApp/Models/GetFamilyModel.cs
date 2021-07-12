551baaf8176113845b40dd83ffd738b2a86969f5
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-12 10:34:43 +0430

    adds data validation to Family pages

--- VaxineApp/VaxineApp/Models/GetFamilyModel.cs
+++ VaxineApp/VaxineApp/Models/GetFamilyModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using VaxineApp.Validations;
 
 namespace VaxineApp.Models
 {
@@ -15,6 +18,26 @@ namespace VaxineApp.Models
         public GetFamilyModel()
         {
             Id = new Guid();
+        }
+    }
+    public class FamilyValidator : AbstractValidator<GetFamilyModel>
+    {
+        public FamilyValidator()
+        {
+            RuleFor(f => f.HouseNo).NotEmpty();
+            RuleFor(f => f.ParentName).Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("{PropertyName} is Empty")
+                .Must(BeAValidName).WithMessage("{PropertyName} must be valid characters")
+                .Length(3, 20).WithMessage("Length of {PropertyName} should be between 3 - 20");
+            RuleFor(f => f.PhoneNumber)
+                .Must(PhoneNumberValidator.IsPhoneNumberValid)
+                .WithMessage("Invalid phone number, must start with +93, 0093 or 0 and has 9 digits after it.");
+        }
+        protected bool BeAValidName(string name)
+        {
+            name = name.Replace(" ", "");
+            name = name.Replace("-", "");
+            return name.All(Char.IsLetter);
         }
     }
 }
