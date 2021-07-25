40d8b6ba8e76ed81b58e77da304a90bece3ad528
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-25 17:15:30 +0430

    adds privacy policy new design

--- VaxineApp/VaxineApp/ViewModels/Settings/PrivacyPolicy/PrivacyPolicyViewModel.cs
+++ VaxineApp/VaxineApp/ViewModels/Settings/PrivacyPolicy/PrivacyPolicyViewModel.cs
@@ -4,14 +4,15 @@ using System.IO;
 using System.Net;
 using System.Text;
 using VaxineApp.MVVMHelper;
+using Xam.Forms.Markdown;
 
 namespace VaxineApp.ViewModels.Settings.PrivacyPolicy
 {
     public class PrivacyPolicyViewModel : ViewModelBase
     {
         // Property
-        private string content;
-        public string Content
+        private MarkdownView content;
+        public MarkdownView Content
         {
             get
             {
@@ -33,7 +34,11 @@ namespace VaxineApp.ViewModels.Settings.PrivacyPolicy
             WebClient client = new WebClient();
             Stream stream = client.OpenRead("https://raw.githubusercontent.com/VDTS/docs/main/PrivacyPolicy.md");
             StreamReader reader = new StreamReader(stream);
-            Content = reader.ReadToEnd();
+
+            var view = new MarkdownView();
+            view.Markdown = reader.ReadToEnd();
+            //view.Theme = new DarkMarkdownTheme(); // Default is white, you also modify various values
+            Content = view;
         }
     }
 }
