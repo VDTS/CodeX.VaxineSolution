0c2f2d02aa4fb9e3866b2df5481c044fa7812b20
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-07-27 16:46:11 +0430

    adds SaveAsPDF to FamilyListPage

---
+++ VaxineApp/VaxineApp/AndroidNativeApi/ISave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VaxineApp.AndroidNativeApi
+{
+    public interface ISave
+    {
+        //Method to save document as a file and view the saved document
+        Task SaveAndView(string filename, string contentType, MemoryStream stream);
+    }
+}
