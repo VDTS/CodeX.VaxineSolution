cccdb239ed8436302950ca132a14b2c58b3d68d6
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-02 08:12:05 +0430

    adds appcenter-post-clone script

---
+++ VaxineApp/VaxineApp.Android/appcenter-post-clone.sh
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env bash
+
+APP_FILE=$APPCENTER_SOURCE_DIRECTORY/VaxineApp/VaxineApp/App.xaml.cs
+
+if [ -e "$APP_FILE" ]
+then
+    echo "Updating App center keys in App.xaml.cs"
+    sed -i.bak 's#android=[AndroidAppCenterKey];"#android='$AndroidAppCenterKey';"#' $APP_FILE
+
+    echo "File content:"
+    cat $APP_FILE
+fi
+
+
