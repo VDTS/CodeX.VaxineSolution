2e79896eb15a537d9f56f7be7feeb080abe49440
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-04 16:10:41 +0430

    adds `beta` label to feedback in beta app

---
+++ VaxineApp/VaxineApp/AndroidNativeApi/IPackageName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.AndroidNativeApi
+{
+    public interface IPackageName
+    {
+        string PackageName { get; }
+    }
+}
