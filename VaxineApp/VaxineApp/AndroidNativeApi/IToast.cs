192c72b02931c3d1d92df734df16f142db8743ce
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-05 10:13:10 +0430

    adds Toast messages for `No-feature` and `no-data`

---
+++ VaxineApp/VaxineApp/AndroidNativeApi/IToast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.AndroidNativeApi
+{
+    public interface IToast
+    {
+        void MakeToast(string message);
+    }
+}
