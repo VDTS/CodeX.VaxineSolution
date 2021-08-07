c724ac94ec9b0ef7cadbee2f052760a102e70f6a
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-07 12:27:22 +0430

    adds GithubService to submit issues

---
+++ VaxineApp/VaxineApp/Models/FeedbackModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace VaxineApp.Models
+{
+    public class FeedbackModel
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public Collection<string> Labels { get; set; }
+        public FeedbackModel()
+        {
+            Labels = new Collection<string>();
+        }
+    }
+}
