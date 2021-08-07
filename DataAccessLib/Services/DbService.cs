e69a6da0a8575dbc111b8e04482b111e936a946b
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-07 17:13:49 +0430

    warnings resolved: using statement simplified, add readonly to readonly fields, naming convention

--- DataAccessLib/Services/DbService.cs
+++ DataAccessLib/Services/DbService.cs
@@ -30,24 +30,20 @@ namespace DataAccessLib.Services
                 try
                 {
                     string url = $"{Constants.FirebaseBaseUrl}Kandahar-Area/{Node}.json";
-                    using (var httpClient = new HttpClient())
-                    {
-                        using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
-                        {
-                            request.Content = new StringContent(data);
-                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                    using var httpClient = new HttpClient();
+                    using var request = new HttpRequestMessage(new HttpMethod("POST"), url);
+                    request.Content = new StringContent(data);
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
 
-                            var response = await httpClient.SendAsync(request);
-                            if (response.IsSuccessStatusCode)
-                            {
-                                var responseBody = await response.Content.ReadAsStringAsync();
-                                return JsonConvert.DeserializeObject<JObject>(responseBody).GetValue("name").ToString();
-                            }
-                            else
-                            {
-                                return "Error";
-                            }
-                        }
+                    var response = await httpClient.SendAsync(request);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseBody = await response.Content.ReadAsStringAsync();
+                        return JsonConvert.DeserializeObject<JObject>(responseBody).GetValue("name").ToString();
+                    }
+                    else
+                    {
+                        return "Error";
                     }
                 }
                 catch (Exception ex)
@@ -68,25 +64,21 @@ namespace DataAccessLib.Services
                 try
                 {
                     string url = $"{Constants.FirebaseBaseUrl}Kandahar-Area/{Node}.json";
-                    using (var httpClient = new HttpClient())
-                    {
-                        using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
-                        {
-                            var response = await httpClient.SendAsync(request);
+                    using var httpClient = new HttpClient();
+                    using var request = new HttpRequestMessage(new HttpMethod("GET"), url);
+                    var response = await httpClient.SendAsync(request);
 
-                            if (response.IsSuccessStatusCode)
-                            {
-                                if (response.Content.ToString() == "null")
-                                {
-                                    return "null";
-                                }
-                                return await response.Content.ReadAsStringAsync();
-                            }
-                            else
-                            {
-                                return "Error";
-                            }
+                    if (response.IsSuccessStatusCode)
+                    {
+                        if (response.Content.ToString() == "null")
+                        {
+                            return "null";
                         }
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        return "Error";
                     }
                 }
                 catch (Exception ex)
@@ -106,20 +98,16 @@ namespace DataAccessLib.Services
             {
                 try
                 {
-                    using (var httpClient = new HttpClient())
+                    using var httpClient = new HttpClient();
+                    using var request = new HttpRequestMessage(new HttpMethod("DELETE"), $"{Constants.FirebaseBaseUrl}Kandahar-Area/{Node}.json");
+                    var response = await httpClient.SendAsync(request);
+                    if (response.IsSuccessStatusCode)
                     {
-                        using (var request = new HttpRequestMessage(new HttpMethod("DELETE"), $"{Constants.FirebaseBaseUrl}Kandahar-Area/{Node}.json"))
-                        {
-                            var response = await httpClient.SendAsync(request);
-                            if (response.IsSuccessStatusCode)
-                            {
-                                return "null";
-                            }
-                            else
-                            {
-                                return "Error";
-                            }
-                        }
+                        return "null";
+                    }
+                    else
+                    {
+                        return "Error";
                     }
                 }
                 catch (Exception ex)
@@ -133,23 +121,19 @@ namespace DataAccessLib.Services
         {
             try
             {
-                using (var httpClient = new HttpClient())
-                {
-                    using (var request = new HttpRequestMessage(new HttpMethod("PUT"), $"{Constants.FirebaseBaseUrl}Kandahar-Area/{Node}.json"))
-                    {
-                        request.Content = new StringContent(data);
-                        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("PUT"), $"{Constants.FirebaseBaseUrl}Kandahar-Area/{Node}.json");
+                request.Content = new StringContent(data);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
 
-                        var response = await httpClient.SendAsync(request);
-                        if (response.IsSuccessStatusCode)
-                        {
-                            return "Submit";
-                        }
-                        else
-                        {
-                            return "Error";
-                        }
-                    }
+                var response = await httpClient.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Submit";
+                }
+                else
+                {
+                    return "Error";
                 }
             }
             catch (Exception)
