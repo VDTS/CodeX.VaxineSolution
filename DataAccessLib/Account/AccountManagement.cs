4727bf3ff58e405e16f21721fcfff3002afdb434
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-09 10:14:01 +0430

    Informational Warnings Resolved

--- DataAccessLib/Account/AccountManagement.cs
+++ DataAccessLib/Account/AccountManagement.cs
@@ -58,165 +58,137 @@ namespace DataAccessLib.Account
         // Methdos
         public async Task<string> ChangeAccountPassword(string token, string password)
         {
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), ChangeAccountPasswordRequestUri))
-                {
-                    var r = new ChangeAccountPasswordModel() { idToken = token, password = password, returnSecureToken = true };
-                    string s1 = JsonConvert.SerializeObject(r);
-                    request.Content = new StringContent(s1);
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), ChangeAccountPasswordRequestUri);
+            var r = new ChangeAccountPasswordModel() { idToken = token, password = password, returnSecureToken = true };
+            string s1 = JsonConvert.SerializeObject(r);
+            request.Content = new StringContent(s1);
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "OK";
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Error";
             }
         }
         public async Task<string> ChangeEmail(string email, string token)
         {
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), ChangeEmailRequestUri))
-                {
-                    var r = new ChangeEmailModel() { idToken = token, email = email, returnSecureToken = true };
-                    string s1 = JsonConvert.SerializeObject(r);
-                    request.Content = new StringContent(s1);
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), ChangeEmailRequestUri);
+            var r = new ChangeEmailModel() { idToken = token, email = email, returnSecureToken = true };
+            string s1 = JsonConvert.SerializeObject(r);
+            request.Content = new StringContent(s1);
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "OK";
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Error";
             }
         }
         public async Task<string> SignIn(string email, string password)
         {
             var identityModel = new AccountModels() { email = email, password = password, returnSecureToken = true };
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), SignInRequestUri))
-                {
-                    request.Content = new StringContent(JsonConvert.SerializeObject(identityModel));
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), SignInRequestUri);
+            request.Content = new StringContent(JsonConvert.SerializeObject(identityModel));
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var a = response.Content.ReadAsStringAsync().Result;
-                        var s1 = JsonConvert.DeserializeObject<JObject>(a);
-                        Preferences.Set("UserLocalId", s1.GetValue("localId").ToString());
-                        return s1.GetValue("idToken").ToString();
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var a = response.Content.ReadAsStringAsync().Result;
+                var s1 = JsonConvert.DeserializeObject<JObject>(a);
+                Preferences.Set("UserLocalId", s1.GetValue("localId").ToString());
+                return s1.GetValue("idToken").ToString();
+            }
+            else
+            {
+                return "Error";
             }
         }
         public async Task<string> VerifyEmail(string Token)
         {
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), VerifyEmailRequestUri))
-                {
-                    VerifyEmailModel verifyEmailModel = new VerifyEmailModel() { idToken = Token, requestType = "VERIFY_EMAIL" };
-                    string s1 = JsonConvert.SerializeObject(verifyEmailModel);
-                    request.Content = new StringContent(s1);
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), VerifyEmailRequestUri);
+            VerifyEmailModel verifyEmailModel = new VerifyEmailModel() { idToken = Token, requestType = "VERIFY_EMAIL" };
+            string s1 = JsonConvert.SerializeObject(verifyEmailModel);
+            request.Content = new StringContent(s1);
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "OK";
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Error";
             }
         }
         public async Task<string> SendPasswordResetcode(string email)
         {
-            using (var httpClient = new HttpClient())
-            {
-                SendPasswordResetCodeModel resetCodeRoot = new SendPasswordResetCodeModel() { email = email, requestType = "PASSWORD_RESET" };
-                var resetCodeRootJson = JsonConvert.SerializeObject(resetCodeRoot);
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), SendPasswordResetCodeRequestUri))
-                {
-                    request.Content = new StringContent(resetCodeRootJson);
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            SendPasswordResetCodeModel resetCodeRoot = new SendPasswordResetCodeModel() { email = email, requestType = "PASSWORD_RESET" };
+            var resetCodeRootJson = JsonConvert.SerializeObject(resetCodeRoot);
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), SendPasswordResetCodeRequestUri);
+            request.Content = new StringContent(resetCodeRootJson);
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "OK";
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Error";
             }
         }
         public async Task<string> VerifyPasswordResetCode(string resetCode)
         {
             VerifyPasswordResetCodeModel resetCodeRoot = new VerifyPasswordResetCodeModel() { oobCode = resetCode };
             var JsonData = JsonConvert.SerializeObject(resetCodeRoot);
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), VerifyPasswordResetCodeRequestUri))
-                {
-                    request.Content = new StringContent(JsonData);
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), VerifyPasswordResetCodeRequestUri);
+            request.Content = new StringContent(JsonData);
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "OK";
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Error";
             }
         }
         public async Task<string> ConfirmPasswordReset(string resetCode, string NewPassword)
         {
             ConfirmPasswordResetModel confirmPasswordRoot = new ConfirmPasswordResetModel() { oobCode = resetCode, newPassword = NewPassword };
             var jsonData = JsonConvert.SerializeObject(confirmPasswordRoot);
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), ConfirmPasswordResetRequestUri))
-                {
-                    request.Content = new StringContent(jsonData);
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), ConfirmPasswordResetRequestUri);
+            request.Content = new StringContent(jsonData);
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return "OK";
-                    }
-                    else
-                    {
-                        return "Error";
-                    }
-                }
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "Error";
             }
         }
 
