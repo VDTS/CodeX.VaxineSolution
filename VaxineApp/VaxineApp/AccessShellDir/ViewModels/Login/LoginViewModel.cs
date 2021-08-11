02eba8d3643c823aff3281dbc1d306bf92cf323a
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-11 16:10:11 +0430

    Methods extracted on LoginViewModel

--- VaxineApp/VaxineApp/AccessShellDir/ViewModels/Login/LoginViewModel.cs
+++ VaxineApp/VaxineApp/AccessShellDir/ViewModels/Login/LoginViewModel.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using VaxineApp.AccessShellDir.ViewModels.Login.Commands;
 using VaxineApp.AccessShellDir.Views.Login;
@@ -146,7 +147,16 @@ namespace VaxineApp.AccessShellDir.ViewModels.Login
                     Preferences.Set("UserLocalId", localId);
                     Preferences.Set("ProfileEmail", signInResponse.GetValue("email").ToString());
 
+                    if (RememberMe == true)
+                    {
+                        await Xamarin.Essentials.SecureStorage.SetAsync("isLogged", "1");
+                    }
+
                     LoadProfile(localId);
+                    await LoadCluster();
+                    await LoadTeam();
+                    await LoadVaccinePeriod();
+                    await GoToApp();
                 }
             }
         }
@@ -154,111 +164,179 @@ namespace VaxineApp.AccessShellDir.ViewModels.Login
         private async void LoadProfile(string localId)
         {
 
-            var data = await DataService.Get($"Profile/{localId}");
-            if (data != "Error" && data != "null")
+            var jData = await DataService.Get($"Profile/{localId}");
+            if (jData == "ConnectionError")
             {
-                var clinic = JsonConvert.DeserializeObject<ProfileModel>(data);
-
-                if (clinic.LocalId == Preferences.Get("UserLocalId", ""))
-                {
-                    Preferences.Set("ClusterId", clinic.ClusterId);
-                    Preferences.Set("TeamId", clinic.TeamId);
-                    Preferences.Set("UserId", clinic.Id.ToString());
-                    Preferences.Set("UserName", clinic.FullName);
-                    Preferences.Set("UserRole", clinic.Role);
-                    await Xamarin.Essentials.SecureStorage.SetAsync("Role", clinic.Role);
-                }
-                if (RememberMe == true)
-                {
-                    await Xamarin.Essentials.SecureStorage.SetAsync("isLogged", "1");
-                }
-
-                var clusterData = await DataService.Get($"Cluster");
-                if (data != "Error" && data != "null")
-                {
-                    var cluster = JsonConvert.DeserializeObject<Dictionary<string, ClusterModel>>(clusterData);
-                    foreach (KeyValuePair<string, ClusterModel> item in cluster)
-                    {
-                        if (item.Value.Id.ToString() == Preferences.Get("ClusterId", ""))
-                        {
-                            Preferences.Set("VaccinePeriodId", item.Value.CurrentVaccinePeriodId.ToString());
-                        }
-                    }
-                }
-
-
-
-                var teamVar = await DataService.Get($"Team/{Preferences.Get("ClusterId", "")}");
-                if (teamVar != "null" & teamVar != "Error")
-                {
-                    var teamData = JsonConvert.DeserializeObject<Dictionary<string, TeamModel>>(teamVar);
-                    foreach (KeyValuePair<string, TeamModel> item in teamData)
-                    {
-                        if (item.Value.Id.ToString() == Preferences.Get("TeamId", "").ToString())
-                        {
-                            Team = new TeamModel
-                            {
-                                Id = item.Value.Id,
-                                FId = item.Key.ToString(),
-                                CHWName = item.Value.CHWName,
-                                SocialMobilizerId = item.Value.SocialMobilizerId,
-                                TeamNo = item.Value.TeamNo,
-                                TotalChilds = item.Value.TotalChilds,
-                                TotalClinics = item.Value.TotalClinics,
-                                TotalDoctors = item.Value.TotalDoctors,
-                                TotalHouseholds = item.Value.TotalHouseholds,
-                                TotalInfluencers = item.Value.TotalInfluencers,
-                                TotalMasjeeds = item.Value.TotalMasjeeds,
-                                TotalSchools = item.Value.TotalSchools,
-                                TotalGuestChilds = item.Value.TotalGuestChilds,
-                                TotalRefugeeChilds = item.Value.TotalRefugeeChilds,
-                                TotalReturnChilds = item.Value.TotalReturnChilds
-                            };
-                            StaticDataStore.TeamStats = Team;
-                            Preferences.Set("TeamFId", Team.FId);
-                        }
-                    }
-
-                }
-
-                var VaccinePeriod = await DataService.Get("VaccinePeriods");
-                if (data != "Error" && data != "null")
-                {
-                    var vaccine = JsonConvert.DeserializeObject<Dictionary<string, VaccinePeriods>>(VaccinePeriod);
-                    foreach (KeyValuePair<string, VaccinePeriods> item in vaccine)
-                    {
-                        if (item.Value.Id.ToString() == Preferences.Get("VaccinePeriodId", "").ToString())
-                        {
-                            Preferences.Set("PeriodStartDate", item.Value.StartDate);
-                            Preferences.Set("PeriodEndDate", item.Value.EndDate);
-                        }
-                    }
-                }
-                var role = await Xamarin.Essentials.SecureStorage.GetAsync("Role");
-                if (role == "Mobilizer")
-                {
-                    Application.Current.MainPage = new AppShell();
-                    await Shell.Current.GoToAsync($"//{nameof(StatusPage)}");
-                }
-                else if (role == "Supervisor")
-                {
-                    Application.Current.MainPage = new SupAppShell();
-                    await Shell.Current.GoToAsync($"//{nameof(ProfilePage)}");
-                }
-                else if (role == "Parent")
-                {
-                    Application.Current.MainPage = new ParentAppShell();
-                    await Shell.Current.GoToAsync($"//{nameof(FamilyPage)}");
-                }
-                else
-                {
-                    Application.Current.MainPage = new AccessShell();
-                    await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-                }
+                StandardMessagesDisplay.NoConnectionToast();
+            }
+            else if (jData == "null")
+            {
+                StandardMessagesDisplay.NoDataDisplayMessage();
+            }
+            else if (jData == "Error")
+            {
+                StandardMessagesDisplay.Error();
+            }
+            else if (jData == "ErrorTracked")
+            {
+                StandardMessagesDisplay.ErrorTracked();
             }
             else
             {
-                await App.Current.MainPage.DisplayAlert("Error!", "Profile Not found", "OK");
+                var data = JsonConvert.DeserializeObject<ProfileModel>(jData);
+
+                if (data.LocalId == Preferences.Get("UserLocalId", ""))
+                {
+                    Preferences.Set("ClusterId", data.ClusterId);
+                    Preferences.Set("TeamId", data.TeamId);
+                    Preferences.Set("UserId", data.Id.ToString());
+                    Preferences.Set("UserName", data.FullName);
+                    Preferences.Set("UserRole", data.Role);
+                    await Xamarin.Essentials.SecureStorage.SetAsync("Role", data.Role);
+                }
+
+            }
+        }
+
+        private static async Task GoToApp()
+        {
+            var role = await Xamarin.Essentials.SecureStorage.GetAsync("Role");
+            if (role == "Mobilizer")
+            {
+                Application.Current.MainPage = new AppShell();
+                await Shell.Current.GoToAsync($"//{nameof(StatusPage)}");
+            }
+            else if (role == "Supervisor")
+            {
+                Application.Current.MainPage = new SupAppShell();
+                await Shell.Current.GoToAsync($"//{nameof(ProfilePage)}");
+            }
+            else if (role == "Parent")
+            {
+                Application.Current.MainPage = new ParentAppShell();
+                await Shell.Current.GoToAsync($"//{nameof(FamilyPage)}");
+            }
+            else
+            {
+                Application.Current.MainPage = new AccessShell();
+                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+            }
+        }
+
+        private async Task LoadVaccinePeriod()
+        {
+            var jData = await DataService.Get("VaccinePeriods");
+            if (jData == "ConnectionError")
+            {
+                StandardMessagesDisplay.NoConnectionToast();
+            }
+            else if (jData == "null")
+            {
+                StandardMessagesDisplay.NoDataDisplayMessage();
+            }
+            else if (jData == "Error")
+            {
+                StandardMessagesDisplay.Error();
+            }
+            else if (jData == "ErrorTracked")
+            {
+                StandardMessagesDisplay.ErrorTracked();
+            }
+            else
+            {
+                var data = JsonConvert.DeserializeObject<Dictionary<string, VaccinePeriods>>(jData);
+                foreach (KeyValuePair<string, VaccinePeriods> item in data)
+                {
+                    if (item.Value.Id.ToString() == Preferences.Get("VaccinePeriodId", "").ToString())
+                    {
+                        Preferences.Set("PeriodStartDate", item.Value.StartDate);
+                        Preferences.Set("PeriodEndDate", item.Value.EndDate);
+                    }
+                }
+            }
+        }
+
+        private async Task LoadTeam()
+        {
+            var jData = await DataService.Get($"Team/{Preferences.Get("ClusterId", "")}");
+            if (jData == "ConnectionError")
+            {
+                StandardMessagesDisplay.NoConnectionToast();
+            }
+            else if (jData == "null")
+            {
+                StandardMessagesDisplay.NoDataDisplayMessage();
+            }
+            else if (jData == "Error")
+            {
+                StandardMessagesDisplay.Error();
+            }
+            else if (jData == "ErrorTracked")
+            {
+                StandardMessagesDisplay.ErrorTracked();
+            }
+            else
+            {
+                var data = JsonConvert.DeserializeObject<Dictionary<string, TeamModel>>(jData);
+                foreach (KeyValuePair<string, TeamModel> item in data)
+                {
+                    if (item.Value.Id.ToString() == Preferences.Get("TeamId", "").ToString())
+                    {
+                        Team = new TeamModel
+                        {
+                            Id = item.Value.Id,
+                            FId = item.Key.ToString(),
+                            CHWName = item.Value.CHWName,
+                            SocialMobilizerId = item.Value.SocialMobilizerId,
+                            TeamNo = item.Value.TeamNo,
+                            TotalChilds = item.Value.TotalChilds,
+                            TotalClinics = item.Value.TotalClinics,
+                            TotalDoctors = item.Value.TotalDoctors,
+                            TotalHouseholds = item.Value.TotalHouseholds,
+                            TotalInfluencers = item.Value.TotalInfluencers,
+                            TotalMasjeeds = item.Value.TotalMasjeeds,
+                            TotalSchools = item.Value.TotalSchools,
+                            TotalGuestChilds = item.Value.TotalGuestChilds,
+                            TotalRefugeeChilds = item.Value.TotalRefugeeChilds,
+                            TotalReturnChilds = item.Value.TotalReturnChilds
+                        };
+                        StaticDataStore.TeamStats = Team;
+                        Preferences.Set("TeamFId", Team.FId);
+                    }
+                }
+
+            }
+        }
+
+        private async Task LoadCluster()
+        {
+            var jData = await DataService.Get($"Cluster");
+            if (jData == "ConnectionError")
+            {
+                StandardMessagesDisplay.NoConnectionToast();
+            }
+            else if (jData == "null")
+            {
+                StandardMessagesDisplay.NoDataDisplayMessage();
+            }
+            else if (jData == "Error")
+            {
+                StandardMessagesDisplay.Error();
+            }
+            else if (jData == "ErrorTracked")
+            {
+                StandardMessagesDisplay.ErrorTracked();
+            }
+            else
+            {
+                var cluster = JsonConvert.DeserializeObject<Dictionary<string, ClusterModel>>(jData);
+                foreach (KeyValuePair<string, ClusterModel> item in cluster)
+                {
+                    if (item.Value.Id.ToString() == Preferences.Get("ClusterId", ""))
+                    {
+                        Preferences.Set("VaccinePeriodId", item.Value.CurrentVaccinePeriodId.ToString());
+                    }
+                }
             }
         }
 
