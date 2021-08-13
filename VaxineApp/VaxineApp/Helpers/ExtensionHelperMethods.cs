1285d3171772d0b9c9a72417e2bdf24a0e0e90b5
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-08-13 15:15:24 +0430

    adds AreEmpty() Extension method

---
+++ VaxineApp/VaxineApp/Helpers/ExtensionHelperMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.Helpers
+{
+    public static class ExtensionHelperMethods
+    {
+        public static bool AreEmpty<T>(this T modelClass)
+        {
+            if (modelClass is object)
+            {
+                Type modelClassObj = modelClass.GetType();
+                return (bool)modelClassObj.GetMethod("IsEmpty").Invoke(modelClass, null);
+            }
+            return true;
+        }
+    }
+}
