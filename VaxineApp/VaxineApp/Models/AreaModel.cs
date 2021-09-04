9da6a35a13ee873f57101e62ce954ff69fd8ffbd
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-04 09:30:00 +0430

    IsActive and VaccineStatus moved to Enums folder

--- VaxineApp/VaxineApp/Models/AreaModel.cs
+++ VaxineApp/VaxineApp/Models/AreaModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VaxineApp.Models.Enums;
 
 namespace VaxineApp.Models
 {
--- VaxineApp/VaxineApp/Models/Enums/IsActive.cs
+++ VaxineApp/VaxineApp/Models/Enums/IsActive.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace VaxineApp.Models
+namespace VaxineApp.Models.Enums
 {
     public enum IsActive
     {
