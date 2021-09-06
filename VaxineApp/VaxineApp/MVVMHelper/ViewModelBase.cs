d2837ebcb87da0037c1652be0d81243e789d04c2
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-06 09:35:59 +0430

    RealCacheLib moved to its own project

--- VaxineApp/VaxineApp/MVVMHelper/ViewModelBase.cs
+++ VaxineApp/VaxineApp/MVVMHelper/ViewModelBase.cs
@@ -1,12 +1,10 @@
 ﻿using DataAccessLib.Services;
-using RealCacheLib;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
-using VaxineApp.RealCacheLib;
 using Xamarin.Forms;
 
 namespace VaxineApp.MVVMHelper
@@ -16,8 +14,6 @@ namespace VaxineApp.MVVMHelper
         // This will be removed after moving codebase to new logic
         protected DbService DataService = new DbService(Constants.FirebaseBaseUrl, Constants.FirebaseApiKey);
 
-        protected SQLiteCache sqliteDataCache = new SQLiteCache();
-        protected RequestsHandler requestsHandler = new RequestsHandler();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
