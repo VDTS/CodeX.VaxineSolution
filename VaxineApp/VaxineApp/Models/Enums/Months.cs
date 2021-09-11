a420ee44ab1738461d22e84dae892798399a39c8
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-11 12:38:14 +0430

    adds DateTime converter to message datetime

---
+++ VaxineApp/VaxineApp/Models/Enums/Months.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.Models.Enums
+{
+    public enum Months
+    {
+        Jan,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        Jul,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    }
+}
