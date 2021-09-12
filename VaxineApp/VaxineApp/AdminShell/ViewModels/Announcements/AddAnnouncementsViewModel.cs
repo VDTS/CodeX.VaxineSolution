3e267e0aa22dd4c2b4527a5d8c86fceeae9100de
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-12 08:33:09 +0430

    adds AddAnnouncementPage functionalities

--- VaxineApp/VaxineApp/AdminShell/ViewModels/Announcements/AddAnnouncementsViewModel.cs
+++ VaxineApp/VaxineApp/AdminShell/ViewModels/Announcements/AddAnnouncementsViewModel.cs
@@ -1,10 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
+using System.Windows.Input;
+using VaxineApp.Models;
+using VaxineApp.Models.Enums;
+using VaxineApp.MVVMHelper;
+using VaxineApp.StaticData;
+using Xamarin.Forms;
 
 namespace VaxineApp.AdminShell.ViewModels.Announcements
 {
-    class AddAnnouncementsViewModel
+    public class AddAnnouncementsViewModel : ViewModelBase
     {
+        AnnouncementsValidator ValidationRules { get; set; }
+        // Property
+        private AnnouncementsModel announcement;
+        public AnnouncementsModel Announcement
+        {
+            get
+            {
+                return announcement;
+            }
+            set
+            {
+                announcement = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Commands
+        public ICommand PostCommand { private set; get; }
+
+        // ctor
+        public AddAnnouncementsViewModel()
+        {
+            // Property
+            Announcement = new AnnouncementsModel();
+            ValidationRules = new AnnouncementsValidator();
+
+            // Command
+            PostCommand = new Command(Post);
+        }
+
+        public async void Post()
+        {
+            var result = ValidationRules.Validate(Announcement);
+            Announcement.Id = Guid.NewGuid();
+            Announcement.IsActive = IsActive.Active;
+            Announcement.MessageDateTime = DateTime.UtcNow;
+
+            if (result.IsValid)
+            {
+                var jData = JsonConvert.SerializeObject(Announcement);
+
+                string postResponse = await DataService.Post(jData, "Announcements");
+
+                if (postResponse == "ConnectionError")
+                {
+                    StandardMessagesDisplay.NoConnectionToast();
+                }
+                else if (postResponse == "Error")
+                {
+                    StandardMessagesDisplay.Error();
+                }
+                else if (postResponse == "ErrorTracked")
+                {
+                    StandardMessagesDisplay.ErrorTracked();
+                }
+                else
+                {
+                    var route = "..";
+                    await Shell.Current.GoToAsync(route);
+                }
+            }
+            else
+            {
+                StandardMessagesDisplay.ValidationRulesViolation(result.Errors[0].PropertyName, result.Errors[0].ErrorMessage);
+            }
+        }
     }
 }
