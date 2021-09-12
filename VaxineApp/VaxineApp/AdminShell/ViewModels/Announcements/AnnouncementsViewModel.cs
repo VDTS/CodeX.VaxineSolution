4e2148ba990c364050d9a864c8d96823a6cfa064
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-12 08:33:09 +0430

    adds AddAnnouncementPage functionalities

--- VaxineApp/VaxineApp/AdminShell/ViewModels/Announcements/AnnouncementsViewModel.cs
+++ VaxineApp/VaxineApp/AdminShell/ViewModels/Announcements/AnnouncementsViewModel.cs
@@ -1,10 +1,166 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UtilityLib.Extensions;
+using VaxineApp.AdminShell.Views.Announcements;
+using VaxineApp.Models;
+using VaxineApp.MVVMHelper;
+using VaxineApp.StaticData;
+using Xamarin.Forms;
 
 namespace VaxineApp.AdminShell.ViewModels.Announcements
 {
-    class AnnouncementsViewModel
+    public class AnnouncementsViewModel : ViewModelBase
     {
+        // Property
+        private AnnouncementsModel selectAnnouncements;
+        public AnnouncementsModel SelectAnnouncements
+        {
+            get
+            {
+                return selectAnnouncements;
+            }
+            set
+            {
+                selectAnnouncements = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<AnnouncementsModel> announcements;
+        public ObservableCollection<AnnouncementsModel> Announcements
+        {
+            get
+            {
+                return announcements;
+            }
+            set
+            {
+                announcements = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Commands
+        public ICommand GoToPostPageCommand { private set; get; }
+        public ICommand SaveAsPDFCommand { private set; get; }
+        public ICommand PullRefreshCommand { private set; get; }
+        public ICommand GoToDetailsPageCommand { private set; get; }
+
+        // ctor
+        public AnnouncementsViewModel()
+        {
+            // Property
+            Announcements = new ObservableCollection<AnnouncementsModel>();
+            SelectAnnouncements = new AnnouncementsModel();
+
+            // Get
+            Get();
+
+
+            // Command
+            SaveAsPDFCommand = new Command(SaveAsPDF);
+            PullRefreshCommand = new Command(Refresh);
+            GoToPostPageCommand = new Command(GoToPostPage);
+            GoToDetailsPageCommand = new Command(GoToDetailsPage);
+        }
+
+        public async void GoToDetailsPage()
+        {
+            if (SelectAnnouncements.AreEmpty())
+            {
+                return;
+            }
+            else
+            {
+                var SelectedItemJson = JsonConvert.SerializeObject(SelectAnnouncements);
+                var route = $"{nameof(AnnouncementsDetailsPage)}?Announcement={SelectedItemJson}";
+                await Shell.Current.GoToAsync(route);
+                SelectAnnouncements = null;
+            }
+        }
+
+
+        private void SaveAsPDF(object obj)
+        {
+            StandardMessagesDisplay.FeatureUnderConstructionTitleDisplayMessage();
+        }
+
+        public async void Get()
+        {
+            var jData = await DataService.Get($"Announcements");
+
+            if (jData == "ConnectionError")
+            {
+                StandardMessagesDisplay.NoConnectionToast();
+            }
+            else if (jData == "null")
+            {
+                StandardMessagesDisplay.NoDataDisplayMessage();
+            }
+            else if (jData == "Error")
+            {
+                StandardMessagesDisplay.Error();
+            }
+            else if (jData == "ErrorTracked")
+            {
+                StandardMessagesDisplay.ErrorTracked();
+            }
+            else
+            {
+                var data = JsonConvert.DeserializeObject<Dictionary<string, AnnouncementsModel>>(jData);
+                foreach (KeyValuePair<string, AnnouncementsModel> item in data)
+                {
+                    Announcements.Add(
+                        new AnnouncementsModel
+                        {
+                            Id = item.Value.Id,
+                            Content = item.Value.Content,
+                            IsActive = item.Value.IsActive,
+                            MessageDateTime = item.Value.MessageDateTime,
+                            Title = item.Value.Title,
+                            FId = item.Key
+                        }
+                        );
+                }
+            }
+        }
+
+        async void GoToPostPage()
+        {
+            var route = $"{nameof(AddAnnouncementPage)}";
+            await Shell.Current.GoToAsync(route);
+        }
+        async void Refresh()
+        {
+            IsBusy = true;
+
+            Clear();
+            Get();
+            await Task.Delay(2000);
+
+            IsBusy = false;
+        }
+
+        void Clear()
+        {
+            Announcements.Clear();
+        }
     }
 }
