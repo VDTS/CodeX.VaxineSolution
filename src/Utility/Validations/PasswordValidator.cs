2aef28be71a4cc0d1d1049ef0284cd2468416377
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-20 11:04:27 +0430

    adds NewPasswordModel, refactor code

---
+++ src/Utility/Validations/PasswordValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Utility.Validations
+{
+    public static class PasswordValidator
+    {
+        public static bool IsPasswordValid(string password)
+        {
+            if (!string.IsNullOrEmpty(password))
+            {
+                string passwordRegex = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$";
+                return Regex.IsMatch(password, passwordRegex);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
