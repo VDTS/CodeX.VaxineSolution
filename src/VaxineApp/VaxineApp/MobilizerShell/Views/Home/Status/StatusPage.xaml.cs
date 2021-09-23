102022e4c6abdcf01b29bb686cc452018ea61d5a
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-23 14:43:41 +0430

    adds eventhandler to load data in status and anonymous child page

--- src/VaxineApp/VaxineApp/MobilizerShell/Views/Home/Status/StatusPage.xaml.cs
+++ src/VaxineApp/VaxineApp/MobilizerShell/Views/Home/Status/StatusPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using VaxineApp.MobilizerShell.ViewModels.Home.Status;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VaxineApp.MobilizerShell.Views.Home.Status
@@ -6,9 +8,29 @@ namespace VaxineApp.MobilizerShell.Views.Home.Status
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StatusPage : ContentPage
     {
+        public delegate void OnAppearingEventHandler(object sender, EventArgs e);
+
+        public event OnAppearingEventHandler PageAppearing;
+
         public StatusPage()
         {
             InitializeComponent();
+
+            var svm = ((StatusViewModel)this.BindingContext);
+            this.PageAppearing += svm.FirstLoad;
         }
+
+        protected override void OnAppearing()
+        {
+            OnPageAppearing();
+        }
+        protected virtual void OnPageAppearing()
+        {
+            if (PageAppearing != null)
+            {
+                PageAppearing(this, EventArgs.Empty);
+            }
+        }
+
     }
 }
