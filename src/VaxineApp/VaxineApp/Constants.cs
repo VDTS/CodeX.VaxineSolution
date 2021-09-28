7775a972918bdcf5589e47a62f9072fe5b9af02e
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-28 17:26:16 +0430

    adds constants and script to add constants

--- src/VaxineApp/VaxineApp/Constants.cs
+++ src/VaxineApp/VaxineApp/Constants.cs
@@ -15,5 +15,18 @@ namespace VaxineApp
         // Firebase keys
         public static string FirebaseApiKey = "FirebaseApiKeyPlaceholder";
         public static string FirebaseBaseUrl = "FirebaseBaseUrlPlaceholder";
+
+
+        // Firebase Service Private Key Constants
+        public static string AuthProviderX509CertUrl = "AuthProviderX509CertUrlPlaceholder";
+        public static string AuthUri = "AuthUriPlaceholder";
+        public static string ClientEmail = "ClientEmailPlaceholder";
+        public static string ClientId = "ClientIdPlaceholder";
+        public static string ClientX509CertUrl = "ClientX509CertUrlPlaceholder";
+        public static string PrivateKey = "PrivateKeyPlaceholder";
+        public static string PrivateKeyId = "PrivateKeyIdPlaceholder";
+        public static string ProjectId = "ProjectIdPlaceholder";
+        public static string TokenUri = "TokenUriPlaceholder";
+        public static string Type = "TypePlaceholder";
     }
 }
