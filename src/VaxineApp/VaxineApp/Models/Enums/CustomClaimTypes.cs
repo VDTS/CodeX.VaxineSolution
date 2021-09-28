0d147c7ac36e7bc1fa6456afce63d4126efcb75e
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-28 16:27:17 +0430

    adds PrivateKeyModel, FirebaseUserModel and CustomClaimTypes to manage users

---
+++ src/VaxineApp/VaxineApp/Models/Enums/CustomClaimTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace VaxineApp.Models.Enums
+{
+    public static class CustomClaimTypes
+    {
+        public const string Role = "Role";
+        public const string FamilyId = "FamilyId";
+        public const string TeamId = "TeamId";
+        public const string ClusterId = "ClusterId";
+    }
+}
