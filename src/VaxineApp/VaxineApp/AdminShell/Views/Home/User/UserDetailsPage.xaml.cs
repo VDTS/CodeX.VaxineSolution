9483b23869036ae4cd6ae43741112b0e5efd5add
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 08:21:36 +0430

    adds UserDetailsPage

--- src/VaxineApp/VaxineApp/AdminShell/Views/Home/User/UserDetailsPage.xaml.cs
+++ src/VaxineApp/VaxineApp/AdminShell/Views/Home/User/UserDetailsPage.xaml.cs
@@ -1,15 +1,29 @@
 ﻿
+using Newtonsoft.Json;
+using VaxineApp.AdminShell.ViewModels.Home.User;
+using VaxineApp.Models.AccountModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VaxineApp.AdminShell.Views.Home.User
 {
+    [QueryProperty(nameof(User), nameof(User))]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UserDetailsPage : ContentPage
     {
+        public string? User { get; set; }
         public UserDetailsPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (User != null)
+            {
+                var result = JsonConvert.DeserializeObject<FirebaseUserModel>(User);
+                if (result != null) BindingContext = new UserDetailsViewModel(result);
+            }
         }
     }
 }
