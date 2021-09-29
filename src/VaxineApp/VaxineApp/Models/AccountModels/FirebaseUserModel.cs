eae13a955fcfde0a285b052a48ed23fda32e6ba5
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 13:07:39 +0430

    adds fields to user details page

--- src/VaxineApp/VaxineApp/Models/AccountModels/FirebaseUserModel.cs
+++ src/VaxineApp/VaxineApp/Models/AccountModels/FirebaseUserModel.cs
@@ -6,5 +6,6 @@
         public string Role { get; set; }
         public string Email { get; set; }
         public string UId { get; set; }
+        public bool EmailVerified {  get; set; }
     }
 }
