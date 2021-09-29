1090ddb12f7d49b671ecc2968ad8a7ef9de10ec5
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 16:20:57 +0430

    adds behaviors and validations for phone and email

---
+++ src/Utility/Validations/PhonuNumberWithInternationCodeValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Utility.Validations
+{
+    public class PhonuNumberWithInternationCodeValidator
+    {
+        public static bool IsPhoneNumberValid(string? phoneNumber)
+        {
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                string phoneNumberPattern = @"((\+93))[7]\d{8}";
+
+                bool isPhoneNumber = Regex.IsMatch(phoneNumber, phoneNumberPattern);
+
+                if (isPhoneNumber)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (string.IsNullOrEmpty(phoneNumber))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
