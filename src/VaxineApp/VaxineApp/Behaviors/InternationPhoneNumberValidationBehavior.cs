39c71267d9a709526b47040570cf7e22a8971b75
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 16:20:57 +0430

    adds behaviors and validations for phone and email

---
+++ src/VaxineApp/VaxineApp/Behaviors/InternationPhoneNumberValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utility.Validations;
+using Xamarin.Forms;
+
+namespace VaxineApp.Behaviors
+{
+    public class InternationPhoneNumberValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry emailEntry)
+        {
+            emailEntry.TextChanged += PhoneNumberEntryChanged;
+            base.OnAttachedTo(emailEntry);
+        }
+
+        private void PhoneNumberEntryChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = (Entry)sender;
+            if (PhonuNumberWithInternationCodeValidator.IsPhoneNumberValid(entry.Text))
+            {
+                entry.TextColor = Color.Black;
+            }
+            else
+            {
+                entry.TextColor = Color.Red;
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry emailEntry)
+        {
+            emailEntry.TextChanged += PhoneNumberEntryChanged;
+            base.OnDetachingFrom(emailEntry);
+        }
+    }
+}
