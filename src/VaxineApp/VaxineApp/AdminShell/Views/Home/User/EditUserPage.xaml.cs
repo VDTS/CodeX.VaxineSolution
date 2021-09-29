51d764b68547f9aae6a9f483877a0a7f56fdc7f1
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 08:36:10 +0430

    adds EditClaims to EditUser page

--- src/VaxineApp/VaxineApp/AdminShell/Views/Home/User/EditUserPage.xaml.cs
+++ src/VaxineApp/VaxineApp/AdminShell/Views/Home/User/EditUserPage.xaml.cs
@@ -1,15 +1,29 @@
 ﻿
+using Newtonsoft.Json;
+using VaxineApp.AdminShell.ViewModels.Home.User;
+using VaxineApp.Models.AccountModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VaxineApp.AdminShell.Views.Home.User
 {
+    [QueryProperty(nameof(User), nameof(User))]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EditUserPage : ContentPage
     {
+        public string? User { get; set; }
         public EditUserPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (User != null)
+            {
+                var result = JsonConvert.DeserializeObject<FirebaseUserModel>(User);
+                if (result != null) BindingContext = new EditUserViewModel(result);
+            }
         }
     }
 }
