5ae54dd79effd3444b552e435b064caf530310d6
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 16:26:03 +0430

    adds PasswordGenerator `WIP`

---
+++ src/Utility/Generators/PasswordGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Generators
+{
+    public static class PasswordGenerator
+    {
+        // This class needs implementation
+        public static string GeneratePassword(int passwordLength = 8,
+            bool alphabet = true,
+            bool numbers = true,
+            bool punctuations = true,
+            bool capitals = true)
+        {
+            return "Ab@32!#l3";
+        }
+    }
+}
