21098965f500e0c7e871aa3002a0c1f39c00a04d
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-09-29 07:48:05 +0430

    adds UserPage to list Users

--- src/VaxineApp/VaxineApp/Models/AccountModels/FirebaseUserModel.cs
+++ src/VaxineApp/VaxineApp/Models/AccountModels/FirebaseUserModel.cs
@@ -5,5 +5,6 @@
         public string DisplayName { get; set; }
         public string Role { get; set; }
         public string Email { get; set; }
+        public string UId { get; set; }
     }
 }
