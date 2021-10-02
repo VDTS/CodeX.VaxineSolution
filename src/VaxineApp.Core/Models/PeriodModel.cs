8bad054c4c9c1e1836998c1956010451b2a9cd28
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-10-02 15:56:37 +0430

    adds PeriodList, Edit, Delete and Add

---
+++ src/VaxineApp.Core/Models/PeriodModel.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using System;
+
+namespace VaxineApp.Core.Models
+{
+    public class PeriodModel
+    {
+        public string FId { get; set; }
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? PeriodName { get; set; }
+        public PeriodModel()
+        {
+            Id = new Guid();
+        }
+
+        public bool IsEmpty()
+        {
+            if (Id == Guid.Empty && StartDate == default && EndDate == default && PeriodName is null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public class PeriodValidator : AbstractValidator<PeriodModel>
+    {
+        public PeriodValidator()
+        {
+            RuleFor(p => p.PeriodName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("{PropertyName} is Empty")
+                .Length(1, 20).WithMessage("Length of {PropertyName} should be between 1 - 20");
+            RuleFor(p => p.StartDate)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty();
+            RuleFor(p => p.EndDate)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("{PropertyName} is Empty");
+        }
+    }
+}
--- src/VaxineApp/VaxineApp/AdminShell/ViewModels/Home/Period/AddPeriodViewModel.cs
+++ src/VaxineApp/VaxineApp/AdminShell/ViewModels/Home/Period/AddPeriodViewModel.cs
@@ -1,6 +1,81 @@
-﻿namespace VaxineApp.AdminShell.ViewModels.Home.Period
+﻿using Newtonsoft.Json;
+using System;
+using System.Windows.Input;
+using VaxineApp.Core.Models;
+using VaxineApp.MVVMHelper;
+using VaxineApp.StaticData;
+using Xamarin.Forms;
+
+namespace VaxineApp.AdminShell.ViewModels.Home.Period
 {
-    class AddPeriodViewModel
+    public class AddPeriodViewModel : ViewModelBase
     {
+        PeriodValidator? ValidationRules { get; set; }
+        // Property
+        private PeriodModel? vaccinePeriod;
+        public PeriodModel? VaccinePeriod
+        {
+            get
+            {
+                return vaccinePeriod;
+            }
+            set
+            {
+                vaccinePeriod = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Command
+        public ICommand PostCommand { private set; get; }
+
+        public AddPeriodViewModel()
+        {
+            VaccinePeriod = new PeriodModel();
+            ValidationRules = new PeriodValidator();
+
+            // Command
+            PostCommand = new Command(Post);
+        }
+        private async void Post()
+        {
+            if (VaccinePeriod != null)
+            {
+                VaccinePeriod.Id = Guid.NewGuid();
+                VaccinePeriod.StartDate = VaccinePeriod.StartDate.ToUniversalTime();
+                VaccinePeriod.EndDate = VaccinePeriod.EndDate.ToUniversalTime();
+
+                var result = ValidationRules?.Validate(VaccinePeriod);
+                if (result != null && result.IsValid)
+                {
+                    var jData = JsonConvert.SerializeObject(VaccinePeriod);
+
+                    string postResponse = await DataService.Post(jData, $"VaccinePeriods");
+                    if (postResponse == "ConnectionError")
+                    {
+                        StandardMessagesDisplay.NoConnectionToast();
+                    }
+                    else if (postResponse == "Error")
+                    {
+                        StandardMessagesDisplay.Error();
+                    }
+                    else if (postResponse == "ErrorTracked")
+                    {
+                        StandardMessagesDisplay.ErrorTracked();
+                    }
+                    else
+                    {
+                        StandardMessagesDisplay.AddDisplayMessage(VaccinePeriod.PeriodName);
+
+                        var route = "..";
+                        await Shell.Current.GoToAsync(route);
+                    }
+                }
+                else
+                {
+                    StandardMessagesDisplay.ValidationRulesViolation(result?.Errors[0].PropertyName, result?.Errors[0].ErrorMessage);
+                }
+            }
+        }
     }
 }
