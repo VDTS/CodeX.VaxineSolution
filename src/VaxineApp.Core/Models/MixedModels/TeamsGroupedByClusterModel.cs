172de8fda7526dfc40696b3754c720ca53922189
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-10-02 12:34:11 +0430

    adds teams page

---
+++ src/VaxineApp.Core/Models/MixedModels/TeamsGroupedByClusterModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VaxineApp.Core.Models.MixedModels
+{
+    public class TeamsGroupedByClusterModel : List<TeamModel>
+    {
+        public string ClusterName { get; private set; }
+        public TeamsGroupedByClusterModel(string clusterName, List<TeamModel> Teams) : base(Teams)
+        {
+            this.ClusterName = clusterName;
+        }
+    }
+}
