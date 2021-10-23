90c89b016f50347a4fdc39daec69437ca7b0b2e3
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-10-23 11:34:48 +0430

    adds Parent, Report, and Insights Pages to Supervisor

---
+++ src/VaxineApp/VaxineApp/SupervisorShellDir/ViewModels/Parents/ParentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaxineApp.SupervisorShellDir.ViewModels.Parents
+{
+    public class ParentViewModel
+    {
+    }
+}
